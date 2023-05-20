2b66475a5ca67712f35121c4ca1b2b8d4bf629fb
Juan Carlos Ordóñez Palazón <jucaror24@gmail.com>
2023-05-20 21:04:01 +0200

    Add API call to fetch and display GIF names

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react'
+import  { getGifs } from '../helpers/getGifs'
+
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
 
+    const getImages = async() => {
+        const newImages= await getGifs( category );
+        setImages(newImages);
+    }
+    useEffect(() => {
+      getImages();
+    }, [])
+    
     return (
         <>
             <h3> {category} </h3>
+            <ol>
+                {images.map( ({id, title}) => (
+                    <li key={ id}>{title}</li>
+                ))}
+            </ol>
         </>
     )
 }
